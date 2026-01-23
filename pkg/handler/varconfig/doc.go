453e6e28dd71072963d2964d4b270940ab7b8951
ArlessonMoura <arlessonmss@gmail.com>
2026-01-23 10:06:04 -0300

    🔥remove files or change directories

--- pkg/handler/varconfig/doc.go
+++
@@ -1,7 +0,0 @@
-// Package varconfig provides HTTP handlers for managing variable configurations.
-//
-// This package contains the HTTP layer for the VarConfig CRUD API,
-// including request validation, response formatting, and routing.
-//
-// Handlers interact with the service layer through the VarConfigUseCase interface
-package varconfig
