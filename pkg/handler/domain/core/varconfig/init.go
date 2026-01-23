e2cd08b9c1e761a7811eb4df58bd9c5c4aab3218
ArlessonMoura <arlessonmss@gmail.com>
2026-01-23 10:09:02 -0300

    🍻pkg with wiring, adjust models and methods

---
+++ pkg/handler/domain/core/varconfig/init.go
@@ -0,0 +1,22 @@
+package varconfig
+
+import (
+	// Aliases para evitar confusão entre pacotes de mesmo nome
+	svcvarconfig "projeto-crud-credencials/internal/service/domain/core/varconfig"
+	storagevarconfig "projeto-crud-credencials/internal/storage/dynamodb/varconfig"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+// InitHandler inicializa o handler com todas as dependências (Wiring)
+func InitHandler(client *dynamodb.Client, tableName string) *Handler {
+	// 1. Instancia o repository (Implementação do Driver DynamoDB)
+	repositoryImpl := storagevarconfig.NewRepository(client, tableName)
+
+	// 2. Instancia o service injetando o repository
+	// O serviceImpl deve implementar a interface IVarConfigService definida neste pacote
+	serviceImpl := svcvarconfig.NewService(repositoryImpl)
+
+	// 3. Retorna o handler com o service injetado
+	return NewHandler(serviceImpl)
+}
