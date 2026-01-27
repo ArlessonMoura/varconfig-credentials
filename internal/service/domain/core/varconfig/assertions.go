0ee1414e7cba021c1368cbb102da2a8c666697a0
ArlessonMoura <arlessonmss@gmail.com>
2026-01-27 09:05:00 -0300

    🩹assertions on new ports package

--- internal/service/domain/core/varconfig/assertions.go
+++ internal/service/domain/core/varconfig/assertions.go
@@ -2,3 +2,10 @@ package varconfig
 
 // Service assertions são verificados nos testes unitários
 // para evitar ciclos de importação
+
+import (
+	svcvarconfig "projeto-crud-credencials/pkg/handler"
+)
+
+// assertions garante que Service implementa a interface IVarConfigService
+var _ svcvarconfig.IVarConfigService = (*Service)(nil)
