cfc7aba5a2d9b4f1b9b9dcefa97df799235efffc
ArlessonMoura <arlessonmss@gmail.com>
2026-02-27 11:01:29 -0300

    🎨 a few improvements

--- pkg/handler/domain/compliance/benchmark/handler.go
+++ pkg/handler/domain/compliance/benchmark/handler.go
@@ -20,28 +20,29 @@ func NewHandler(svc ports.IBenchmarkSchemaService) *Handler {
 
 // Handle implementa a interface IRestHandler do restwrapper
 func (h *Handler) Handle(wrapper *restwrapper.Wrapper) {
-	var pathParams PathParams
-	if err := wrapper.RequestWrapper.BindPathParams(&pathParams); err != nil {
-		wrapper.ResponseWrapper.WriteClientErrorResponse(http.StatusBadRequest, err.Error())
-		return
-	}
-
 	method := wrapper.RequestWrapper.Method()
 	switch method {
 	case http.MethodGet:
-		id, exists := wrapper.RequestWrapper.GetPathParam("id")
-		if exists && id != nil && *id != "" {
-			h.GetByID(wrapper, pathParams)
+		benchmarkId, exists := wrapper.RequestWrapper.GetPathParam("benchmarkId")
+		if exists && benchmarkId != nil && *benchmarkId != "" {
+			h.GetByID(wrapper)
 		} else {
-			h.List(wrapper, pathParams)
+			h.List(wrapper)
 		}
 	default:
 		wrapper.ResponseWrapper.WriteClientErrorResponse(http.StatusMethodNotAllowed, "method not allowed")
 	}
 }
 
-func (h *Handler) GetByID(wrapper *restwrapper.Wrapper, pathParams PathParams) {
+func (h *Handler) GetByID(wrapper *restwrapper.Wrapper) {
 	ctx := context.Background()
+
+	// Binding dos path parameters necessários para este endpoint
+	var pathParams PathParams
+	if err := wrapper.RequestWrapper.BindPathParams(&pathParams); err != nil {
+		wrapper.ResponseWrapper.WriteClientErrorResponse(http.StatusBadRequest, err.Error())
+		return
+	}
 
 	schema, err := h.svc.GetByID(ctx, pathParams.BenchmarkID)
 	if err != nil {
@@ -55,9 +56,11 @@ func (h *Handler) GetByID(wrapper *restwrapper.Wrapper, pathParams PathParams) {
 	wrapper.ResponseWrapper.WriteSuccessResponse(http.StatusOK, schema)
 }
 
-func (h *Handler) List(wrapper *restwrapper.Wrapper, pathParams PathParams) {
+func (h *Handler) List(wrapper *restwrapper.Wrapper) {
 	ctx := context.Background()
 
+	// Para List não precisamos de path parameters obrigatórios
+	// Chamamos diretamente o service
 	schemas, err := h.svc.List(ctx)
 	if err != nil {
 		wrapper.ResponseWrapper.WriteServerErrorResponse()
